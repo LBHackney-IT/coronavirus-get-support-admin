aa4157f8d3e7f99f8c66ea9e3cb2338cd7e2d562
Jayson Hunter <github@jaysonhunter.com>
2020-05-05 10:38:34 +0100

    Secure routes with auth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const authCheck = require('../helpers/auth');
+
 // GET request
-router.get('/', function(req, res) {
+router.get('/', authCheck, function(req, res) {
     res.locals.query = req.query;
     res.locals.addresses_api_url = process.env.ADDRESSES_API_URL;
     res.locals.addresses_api_key = process.env.ADDRESSES_API_KEY;
