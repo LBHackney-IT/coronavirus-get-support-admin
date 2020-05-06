319f8c76fb07efab55a604dfa606409baf09ca6f
Jayson Hunter <github@jaysonhunter.com>
2020-05-06 12:12:41 +0100

    Update the login return url, and in no token render the login screen. (#6)

--- helpers/auth.js
+++ helpers/auth.js
@@ -3,22 +3,19 @@ const jwt = require('jsonwebtoken');
 const config = require('../config');
 
 const isAuthorised = (req, res, next) => {
-
   const token = req.cookies[config.token_name];
+
+  res.locals.returnURL = req.hostname;
 
   if (token) {
     let payload = null;
 
     try {
       payload = jwt.verify(token, config.hackney_jwt_secret);
-      console.log(payload);
-
       const groups = payload.groups;
 
       if (groups && groups.includes(config.authorised_group)) {
         return next();
-      } else {
-        return res.status(401).redirect("login.njk", {unauthorised: true});
       }
 
     } catch (err) {
@@ -27,10 +24,12 @@ const isAuthorised = (req, res, next) => {
         } else {
           const error = new Error(err);
 
-           return next(error);
+          return next(error);
         }
     } 
   }
+
+  return res.render("login.njk");
 }
 
 module.exports = isAuthorised;
