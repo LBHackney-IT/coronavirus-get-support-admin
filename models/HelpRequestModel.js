5062a300dede42adf6b98b2e7178c9c1644eec6c
Jayson Hunter <github@jaysonhunter.com>
2020-05-13 08:58:52 +0100

    Add fetchAllExceptions function to help requests model

--- models/HelpRequestModel.js
+++ models/HelpRequestModel.js
@@ -96,6 +96,36 @@ class HelpRequestModel {
             return (err)
         }
     }
+
+
+    /**
+     * @description Fetch all exception records
+     * @returns {Promise<*>}
+     */
+    async fetchAllExceptions() {
+
+        try {
+            let data = [];
+
+            const headers = {
+                "Content-Type": "application/json",
+                "x-api-key": config.api_key
+            };
+
+            await axios.get(config.api_url + '/exceptions', {
+                headers: headers
+            }).then ( result => {
+                data = result;
+            });
+
+            return data;
+
+        } catch (err) {
+            console.log('ExceptionsModel fetchAllExceptions ERR');
+            console.log(err)
+            return (err)
+        }
+    }
 }
 
 module.exports = new HelpRequestModel;
