fea602ee2a82feccb6011c557e880e8f10a7e9f5
Jayson Hunter <github@jaysonhunter.com>
2020-05-13 08:58:10 +0100

    Add route to get help request with UPRN

--- routes/exceptions.js
+++ routes/exceptions.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const exceptionsController = require('../controllers/exceptions');
+const authCheck = require('../helpers/auth');
 
-// POST request to get all help requests
+// GET request to get all exceptions
 router.get('/', exceptionsController.all_exceptions_get);
+
+// GET request to display all help requests by UPRN
+router.get('/:uprn', authCheck, exceptionsController.all_help_requests_get);
 
 module.exports = router;
