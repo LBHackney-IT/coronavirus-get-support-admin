2766fad2f56567294e439cf9f213e8e14e8d8fbd
Jayson Hunter <github@jaysonhunter.com>
2020-05-15 14:17:28 +0100

    Add logic to HelpRequetServices to update multiple Help Requests

--- services/HelpRequestsService.js
+++ services/HelpRequestsService.js
@@ -23,6 +23,7 @@ class HelpRequestsService {
         }
     }
 
+
     /**
      * @description Fetch a single help request using the Annex ID
      * @returns {Promise<*>}
@@ -43,8 +44,9 @@ class HelpRequestsService {
         }
     }
 
+
     /**
-     * @description Fetch a single help request using the Annex ID
+     * @description Update a single help request
      * @returns {Promise<*>}
      */
     async updateHelpRequest(id, helpRequestData) {
@@ -64,12 +66,36 @@ class HelpRequestsService {
     }
 
 
+    /**
+     * @description Update multiple help requests
+     * @returns {Promise<*>}
+     */
+    async updateAllHelpRequests(helpRequestsData) {
+        try {
+            let data = [];
+
+            await HelpRequestModel.updateAllHelpRequests(helpRequestsData)
+            .then ( (result) => {
+                data = result;
+            });
+            
+            return data;
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    /**
+     * @description Get all Exception help requests
+     * @returns {Promise<*>}
+     */
     async fetchAllExceptions() {
         try {
             let data = [];
 
             await HelpRequestModel.fetchAllExceptions()
-            .then ( (result) => {
+            .then ( (result) => {                
                 data = result;
             });
 
