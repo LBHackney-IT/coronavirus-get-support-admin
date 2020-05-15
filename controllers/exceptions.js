0c1267d00917b04392dac07eb6503e93a6cb693a
Jayson Hunter <github@jaysonhunter.com>
2020-05-15 14:16:10 +0100

    Add POST logic to exceptions controller to update all Help Requests

--- controllers/exceptions.js
+++ controllers/exceptions.js
@@ -15,7 +15,6 @@ module.exports = {
      * @returns {Promise<*>}
      */
     all_exceptions_get: async (req, res, next) => {
-
         try {
             let data = [];
 
@@ -48,29 +47,39 @@ module.exports = {
      * @returns {Promise<*>}
      */
     all_help_requests_get: async (req, res, next) => {
-
         try {
             let data = [];
 
             await helpRequestService.fetchAllHelpRequests({uprn: req.params.uprn, master: false})
             .then(result => {
+                let recordIDs = [];
+
                 data = result.data;
 
                 if (data.length) {
+                    const dynamicFields = [
+                        "RecordStatus",
+                        "IsDuplicate",
+                        "OngoingFoodNeed",
+                        "ContactTelephoneNumber",
+                        "ContactMobileNumber",
+                        "EmailAddress",
+                        "NumberOfPeopleInHouse",
+                        "DobDay",
+                        "DobMonth",
+                        "DobYear"
+                    ];
+
+                    let formattedCreationDate = {};
+
                     data.forEach(item => {
-                        item["RecordStatus_" + item.Id] = item.RecordStatus;
-                        item["IsDuplicate_" + item.Id] = item.IsDuplicate;
-                        item["OngoingFoodNeed_" + item.Id] = item.OngoingFoodNeed;
-                        item["ContactTelephoneNumber_" + item.Id] = item.ContactTelephoneNumber;
-                        item["ContactMobileNumber_" + item.Id] = item.ContactMobileNumber;
-                        item["EmailAddress_" + item.Id] = item.EmailAddress;
-                        item["NumberOfPeopleInHouse_" + item.Id] = item.NumberOfPeopleInHouse;
+                        recordIDs.push(item.Id);
 
-                        item["DobDay_" + item.Id] = item.DobDay;
-                        item["DobMonth_" + item.Id] = item.DobMonth;
-                        item["DobYear_" + item.Id] = item.DobYear;
+                        dynamicFields.forEach((fieldName) => {
+                            item[fieldName + "_" + item.Id] = item[fieldName];
+                        });
 
-                        const formattedCreationDate = dateHelper.convertDate(item.DateTimeRecorded);
+                        formattedCreationDate = dateHelper.convertDate(item.DateTimeRecorded);
 
                         item.creation_date = formattedCreationDate.concatenated;
 
@@ -82,11 +91,13 @@ module.exports = {
                             item["last_confirmed_food_delivery_year_" + item.Id] = formattedDeliveryDate.year;
                             item["last_confirmed_food_delivery_date_" + item.Id] = formattedDeliveryDate.concatenated;
                         }
+
+                        data.AddressFirstLine = data[0].AddressFirstLine;
+                        data.AddressSecondLine = data[0].AddressSecondLine;
+                        data.Postcode = data[0].Postcode;
                     });
 
-                    data.AddressFirstLine = data[0].AddressFirstLine;
-                    data.AddressSecondLine = data[0].AddressSecondLine;
-                    data.Postcode = data[0].Postcode;
+                    data.recordIDs = recordIDs.join(',');
 
                     res.locals.query = data;
                 };
@@ -99,5 +110,92 @@ module.exports = {
 
             return next(error);
         }
+    },
+
+
+    /**
+     * @description Update a list of help requests
+     * @param req {object} Express req object 
+     * @param res {object} Express res object
+     * @param next {object} Express next object
+     * @returns {Promise<*>}
+     */
+    all_help_requests_post: async (req, res, next) => {
+        res.locals.query = req.body;
+
+        let recIds = req.body.record_ids.split(',');
+
+        const dynamicFields = [
+            "RecordStatus",
+            "ContactTelephoneNumber",
+            "ContactMobileNumber",
+            "EmailAddress",
+            "NumberOfPeopleInHouse",
+            "DobDay",
+            "DobMonth",
+            "DobYear",
+            "IsDuplicate"
+        ];
+
+        // const errors = validator.validationResult(req);
+        const errors = {}
+
+        //!errors.isEmpty()
+        if (1===2 ) {
+            var extractedErrors = {};
+            errors
+                .array()
+                .map(err => (extractedErrors["error_" + err.param] = err.msg));
+           
+            return res.redirect(
+                "exceptions/" + req.body.uprn + "?" +
+                querystring.stringify(extractedErrors) +
+                "&" +
+                querystring.stringify(req.body)
+            );
+        } else {
+            var query = req.body;
+
+            try {
+                let recordsData = [];
+
+                recIds.forEach( id => {
+                    let recFields = {};
+
+                    recFields.id = id;
+
+                    dynamicFields.forEach(field => {
+                        recFields[field] = req.body[field + "_" + id];
+                    });
+
+                    recFields.OngoingFoodNeed = req.body["OngoingFoodNeed_" + id] === 'yes' ? true : false;
+
+                    const lcfdDay = req.body["last_confirmed_delivery_day_" + id];
+                    const lcfdMonth = req.body["last_confirmed_delivery_month_" + id];
+                    const lcfdYear = req.body["last_confirmed_delivery_year_" + id];
+
+                    const lastConfirmedFoodDelivery = new Date(Date.UTC(lcfdYear, lcfdMonth - 1, lcfdDay));
+
+                    recFields.LastConfirmedFoodDelivery = lastConfirmedFoodDelivery.toISOString();
+
+                    recordsData.push(recFields);
+                });
+
+                // Update all the Help Request records
+                await helpRequestService.updateAllHelpRequests(recordsData)
+                .then(result => {
+                    return res.redirect("/exceptions");
+                })  
+
+            } catch (err) {
+                const error = new Error(err);
+    
+                return next(error);
+            }
+
+        
+        
+        }
+
     }
 };
