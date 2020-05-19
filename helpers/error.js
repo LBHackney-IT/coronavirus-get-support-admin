e784d0188abf2bdcf7268b02b060d6df45cfe328
Jayson Hunter <github@jaysonhunter.com>
2020-05-19 10:05:33 +0100

    Add API error handler

--- helpers/error.js
+++ helpers/error.js
@@ -25,7 +25,25 @@ class ErrorHandler extends Error {
     });
   };
 
+  const handleAPIErrorResponse = (error) => {
+    let errorResponse;
+
+    if(error.response && error.response.data) {
+      // I expect the API to handle error responses in valid format
+      errorResponse = error.response.data;
+      // JSON stringify if you need the json and use it later
+    } else if(error.request) {
+      // TO Handle the default error response for Network failure or 404 etc.,
+      errorResponse = error.request.message || error.request.statusText;
+    } else {
+      errorResponse = error.message;
+    }
+    
+    throw new Error(errorResponse);
+  }
+
   module.exports = {
     ErrorHandler,
-    handleError
+    handleError,
+    handleAPIErrorResponse
   }
