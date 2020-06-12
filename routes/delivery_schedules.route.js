47970818b2753f84550588e06a88ad55a3ea4b03
Jayson Hunter <github@jaysonhunter.com>
2020-06-12 14:40:46 +0100

    Updated Delivery Schedule routes

--- routes/delivery_schedules.route.js
+++ routes/delivery_schedules.route.js
@@ -6,16 +6,23 @@ const deliverySchedulesController = require('../controllers/delivery_schedules.c
 const {isAuthorised, isAdmin} = require('../middleware/auth');
 
 // GET request to search delivey schedule
-router.get('/', [isAuthorised, isAdmin], function(req, res) {
-    res.locals.query = req.query;
-    res.render("delivery-schedules.njk");
+router.get('/', [isAuthorised], (req, res, next) => {
+   deliverySchedulesController.delivery_schedule_get(req, res, next)
 });
 
-router.post('/list', [isAuthorised, isAdmin, deliveryLimitValidation], (req, res, next) => {
+// POST request to retrieve the list of records for the next delivery schedule report
+router.post('/list', [isAuthorised, deliveryLimitValidation], (req, res, next) => {
     deliverySchedulesController.delivery_schedule_list_post(req, res, next)
 });
 
-router.post('/confirmed', [isAuthorised, isAdmin, deliveryLimitValidation], (req, res, next) => {
+// POST request to confirm the next delivery schedule report
+router.post('/confirmed', [isAuthorised, deliveryLimitValidation], (req, res, next) => {
     deliverySchedulesController.delivery_schedule_confirmed_post(req, res, next)
 });
+
+// DELETE request to remove the current delivery schedule report
+router.get('/delete/:id', [isAuthorised], (req, res, next) => {
+    deliverySchedulesController.delivery_schedule_delete(req, res, next)
+});
+
 module.exports = router;
