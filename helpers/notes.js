3b2b3f6eb6658aa98e7eaa16df22a01af4e0f43c
Jayson Hunter <github@jaysonhunter.com>
2020-08-03 14:53:55 +0100

    Update notes.js helper

--- helpers/notes.js
+++ helpers/notes.js
@@ -2,7 +2,7 @@ const appendNote = (author, newNote, noteHistory, ) => {
     const noteDate = new Date().toGMTString();
     let updatedNotes = "";
 
-    if(newNote.trim().length) {
+    if(newNote && newNote.trim().length) {
         updatedNotes = author + " : " + noteDate + "\n------------\n" + newNote + "\n------\n\n\n" + noteHistory;
     } else {
         updatedNotes = noteHistory;
