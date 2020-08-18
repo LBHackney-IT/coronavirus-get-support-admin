cfc98c65d275764c68c5847adb4d0a1cf595a31f
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-08-18 12:05:18 +0100

    add missing redirect

--- controllers/help-requests/help_requests.controller.js
+++ controllers/help-requests/help_requests.controller.js
@@ -307,6 +307,7 @@ module.exports = {
                 if(result.snapshots && result.snapshots.length > 0) {
                     console.log("Snapshot found, fore resident {} {}", query.firstName, query.lastName);
                     console.log(`Snapshot found, for resident ${query.firstName}  ${query.lastName}. Opening...`);
+                    return res.redirect(SNAPSHOT_URL + "/snapshots/" + result.snapshots[0].id);
                 } else {
                     console.log(`Snapshot not found, for resident ${query.firstName}  ${query.lastName}. Creating...`);
                     HelpRequestsService.createVulnerabilitySnapshot(requestModel, userName)
