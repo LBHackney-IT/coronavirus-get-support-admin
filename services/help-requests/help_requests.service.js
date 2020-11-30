29134d393973e571d8fd7c2b20a773da4a3398b4
Maysa <maysa@madetech.com>
2020-11-30 16:38:20 +0000

    refactor if statement

--- services/help-requests/help_requests.service.js
+++ services/help-requests/help_requests.service.js
@@ -133,20 +133,13 @@ class HelpRequestsService {
       let initialCallBack = query.initialCallBack
       let callbackRequired = query.callbackRequired
 
-      if(query.CallOutcome == "callback_complete"){
+      if(query.CallOutcome == "callback_complete" || query.CallOutcome == "refused_to_engage" || query.CallOutcome == "close_case"){
         initialCallBack = true
         callbackRequired = false
       }else if(Array.isArray(query.CallOutcome) && query.CallOutcome.length == 2 && query.CallOutcome.includes("follow_up_requested") &&  query.CallOutcome.includes("callback_complete")){
         initialCallBack = true
         callbackRequired = true
-      }else if(query.CallOutcome == "refused_to_engage"){
-        initialCallBack = true
-        callbackRequired = false
-      } else if(query.CallOutcome == "close_case"){
-        initialCallBack = true
-        callbackRequired = false
       }
-
       const updatedFields = {
         InitialCallbackCompleted:initialCallBack,
         HelpNeeded: query.HelpNeeded || "",
