c7fbbc1aaa1ec522e3434329c6eb3264be92893a
Kat <katrina@madetech.com>
2020-12-03 12:27:02 +0000

    Fix tests, fix follow up call falling off the callbacks list

--- services/help-requests/help_requests.service.js
+++ services/help-requests/help_requests.service.js
@@ -143,12 +143,15 @@ class HelpRequestsService {
       let initialCallBack = query.initialCallBack
       let callbackRequired = query.callbackRequired
 
-      if(query.CallOutcome == "callback_complete" || query.CallOutcome == "refused_to_engage" || query.CallOutcome == "close_case"){
-        initialCallBack = true
-        callbackRequired = false
-      }else if(Array.isArray(query.CallOutcome) && query.CallOutcome.length == 2 && query.CallOutcome.includes("follow_up_requested") &&  query.CallOutcome.includes("callback_complete")){
-        initialCallBack = true
-        callbackRequired = true
+      if (query.CallOutcome) {
+        if(query.CallOutcome == "callback_complete" || query.CallOutcome == "refused_to_engage" || query.CallOutcome == "close_case"){
+          initialCallBack = true
+          callbackRequired = false
+        }
+        else if(query.CallOutcome.includes("follow_up_requested") || query.CallOutcome.includes("call_rescheduled")){
+          initialCallBack = true
+          callbackRequired = true
+        }
       }
       const updatedFields = {
         InitialCallbackCompleted:initialCallBack,
